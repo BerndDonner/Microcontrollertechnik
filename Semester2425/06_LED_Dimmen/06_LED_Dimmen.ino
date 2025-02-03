ff5b7e61326abc4d4a1086829f30b381f27f2314
BerndDonner <119803636+BerndDonner@users.noreply.github.com>
2025-02-03 09:56:46 +0100

    LED Dimmen vollständig

--- Semester2425/06_LED_Dimmen/06_LED_Dimmen.ino
+++ Semester2425/06_LED_Dimmen/06_LED_Dimmen.ino
@@ -1,6 +1,10 @@
 void setup() {
-  TCCR2A |= (1<<WGM20) | (1<<WGM21); //Wave Generation Mode: 3
-  TCCR2B |= (1<<CS22);               //Prescaler: 64
+  TCCR2A = (1<<WGM20) | (1<<WGM21) | (1 << COM2A1) | (1 << COM2A0); //Wave Generation Mode: 3, Set Pin11 on compare match, clear Pin 11 at 0
+  TCCR2B = (1<<CS22);               //Prescaler: 64
+  
+  OCR2A = 0;
+
+  DDRB |= (1 << PB3);
 
 }
 
