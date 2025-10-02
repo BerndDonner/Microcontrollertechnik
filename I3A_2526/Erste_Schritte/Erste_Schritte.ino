c7e1ba4a22aaf71364a0ec218354ce517f03024b
Bernd Donner <bernd.donner@sabel.com>
2025-10-02 10:34:37 +0200

    2. 10 weitere Schritte

--- I3A_2526/Erste_Schritte/Erste_Schritte.ino
+++ I3A_2526/Erste_Schritte/Erste_Schritte.ino
@@ -6,9 +6,28 @@ void setup()
     Serial.println("Hallo Welt!!!");
 
     int8_t wiesn = 127;
+    int8_t traunsteiner = 0b01111111; // 127 dezimal
+    int8_t augustiner = 0x7f; // 127 dezimal
+
+    uint8_t gaeubodenfest = 127;
+    Serial.print("wiesn: ");
     Serial.println(wiesn);
+    Serial.print("traunsteiner: ");
+    Serial.println(traunsteiner);
+    Serial.print("augustiner: ");
+    Serial.println(augustiner);
+    Serial.print("gaeubodenfest: ");
+    Serial.println(gaeubodenfest);
     wiesn += 1;
-    Serial.println(wiesn);    
+    gaeubodenfest += 1;
+    Serial.print("wiesn nach Erhöhung um 1: ");
+    Serial.println(wiesn);
+    Serial.println(wiesn, BIN);    
+    Serial.println(wiesn, HEX);
+    Serial.print("gaeubodenfest nach Erhöhung um 1: ");
+    Serial.println(gaeubodenfest);
+    Serial.println(gaeubodenfest, BIN);    
+    Serial.println(gaeubodenfest, HEX);    
 }
 
 void loop()
