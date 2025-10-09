d298e55586aa1ab6843945ef8b3952dccba6f33d
Bernd Donner <bernd.donner@sabel.com>
2025-10-09 09:46:15 +0200

    Das Zweierkomplement

--- I3A_2526/Erste_Schritte/Erste_Schritte.ino
+++ I3A_2526/Erste_Schritte/Erste_Schritte.ino
@@ -5,11 +5,18 @@ void setup()
     Serial.begin(115200);
     Serial.println("Hallo Welt!!!");
 
-    int8_t wiesn = 127;
+    int8_t wiesn = 0xff;
     int8_t traunsteiner = 0b01111111; // 127 dezimal
     int8_t augustiner = 0x7f; // 127 dezimal
 
-    uint8_t gaeubodenfest = 127;
+    uint8_t gaeubodenfest = 0xab;
+    uint8_t paulaner = ~gaeubodenfest+1; //Zweierkomplement
+
+    Serial.println("Zweierkomplement: ");
+    Serial.println(gaeubodenfest, BIN);
+    Serial.println(paulaner, BIN);
+    Serial.println((uint8_t) -gaeubodenfest, BIN);
+
     Serial.print("wiesn: ");
     Serial.println(wiesn);
     Serial.print("traunsteiner: ");
