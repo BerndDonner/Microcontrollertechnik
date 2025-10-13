7669692fd4a06410d456bd107cd8f657f584f0e0
Dr. Bernd Donner <119803636+BerndDonner@users.noreply.github.com>
2025-10-13 13:08:07 +0200

    Änderungen in erste Schritte

--- 2526_E4A_Semester/Erste_Schritte/Erste_Schritte.ino
+++ 2526_E4A_Semester/Erste_Schritte/Erste_Schritte.ino
@@ -1,9 +1,19 @@
 // int8_t wiesn;  Dies ist eine global definierte Variable. Vermeiden wenn möglich!
 
+//~a Einserkomplement
+//-a Zweierkomplement -a = (~a+1)
+// a & b  and [logisches und: &&]
+// a | b  or  [logisches oder: ||]
+// a ^ b  xor
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hallo E3A");
+  
+  int8_t benno = 20; //in8_t -128.127!!!
+  Serial.println(benno);  //negative Zahl
+  Serial.println((uint8_t) benno);
+
   for (int8_t wiesn = 123; (uint8_t) wiesn <= 133; ++wiesn) {  //Deklaration und Initialisierung von wiesn
     Serial.print(wiesn);
     Serial.print("\t");
