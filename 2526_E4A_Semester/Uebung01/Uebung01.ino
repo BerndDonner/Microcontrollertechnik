636152d3628ecf5e0b15dd6a786a937b6c0be185
Dr. Bernd Donner <119803636+BerndDonner@users.noreply.github.com>
2025-10-20 12:32:27 +0200

    ignore Schülerordner

--- 2526_E4A_Semester/Uebung01/Uebung01.ino
+++ 2526_E4A_Semester/Uebung01/Uebung01.ino
@@ -5,7 +5,21 @@ void setup() {
   Serial.println("2.te Aufgabe:");
   Serial.println(0xffea, DEC);
   Serial.println("3.te Aufgabe:");
-  Serial.println((uint8_t) 0xff); //<---cast besprechen
+  Serial.println(0xff);
+  Serial.println("4.te Aufgabe:");
+  Serial.println(0xffff);
+  Serial.println("5.te Aufgabe:");
+  Serial.println(0xffffffff);
+  Serial.println("6.te Aufgabe:");
+  Serial.println(sizeof(float));
+  Serial.println("7.te Aufgabe:");
+  Serial.println(sizeof(double));
+  Serial.println("8.te Aufgabe:");
+  Serial.println(0xfa, BIN);
+  Serial.println((uint8_t) ~0xfa, BIN);
+  Serial.println("9.te Aufgabe:");
+  Serial.println("10.te Aufgabe:");
+  
 
   // put your setup code here, to run once:
 
