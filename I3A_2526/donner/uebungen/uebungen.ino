b1473b287652b87e22ac594dad658cdad8494868
Bernd Donner <bernd.donner@sabel.com>
2025-10-23 10:21:10 +0200

    Funktion toHex

--- I3A_2526/donner/uebungen/uebungen.ino
+++ I3A_2526/donner/uebungen/uebungen.ino
@@ -1,15 +1,36 @@
 #include "Arduino.h"
 
-void setup() {
-    Serial.begin(115200);
-    int8_t i = 0xa5;
-    uint8_t u = 0xa5;
+void toHex()
+{
+    char hexa[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-    Serial.println((int8_t) i);
-    Serial.println((uint8_t) u);
-    Serial.println((int16_t) i, HEX);
-    Serial.println((uint16_t) u, HEX);
+    int16_t zahl = 0b0110110000001101;
+    uint16_t uz = (uint16_t)zahl;
+
+    for (uint8_t i = 0; i < 4; ++i)
+    {
+        // 12 high bit Null setzten:
+        uint8_t j = uz & 0b0000000000001111; // 0x000f
+        Serial.print(hexa[j]);
+        uz >>= 4;
+    }
 }
 
-void loop() {
+void promotion()
+{
+    int8_t i = 0xa5;                  // 0b1010'0101  → entspricht -91 im Zweierkomplement
+    uint8_t u = 0xa5;                 // 0b1010'0101  → entspricht 165 als unsigned
+    Serial.println((int16_t)i, HEX);  // ergibt 0xFFA5 (wegen Vorzeichenerweiterung)
+    Serial.println((uint16_t)u, HEX); // ergibt 0x00A5 (wegen Nullenauffüllung)
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    toHex();
+}
+
+void loop()
+{
 }
