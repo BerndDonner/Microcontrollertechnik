568e17026e8f01d02d45b367741c95e0469bbf47
Dr. Bernd Donner <119803636+BerndDonner@users.noreply.github.com>
2025-10-27 12:56:26 +0100

    Analyse einer echten Bitoperation

--- 2526_E4A_Semester/Uebung01/Uebung01.ino
+++ 2526_E4A_Semester/Uebung01/Uebung01.ino
@@ -1,3 +1,5 @@
+#include "donner.h"
+
 void uebungung1_10() {
   Serial.println("1.te Aufgabe:");
   Serial.println(47, HEX);
@@ -26,10 +28,46 @@ void uebungung1_10() {
 
 void setup() {
   Serial.begin(9600);
-  uebungung1_10();
+//  uebungung1_10();
 
   DDRD & ~((1 << PD2) | (1 << PD3)); //<--- TODO
+
+  Serial.print("PD2: ");
+  Serial.println(PD2);
+
+  Serial.print("PD3: ");
+  Serial.println(PD3);
   
+  Serial.print("1:\t\t");
+  printBin((uint8_t) 1);
+  
+  Serial.print("1 << PD2:\t");
+  printBin((uint8_t) (1 << PD2));
+ 
+  Serial.print("1 << PD3:\t");
+  printBin((uint8_t) (1 << PD3));
+
+  Serial.print("(1 << PD2) | (1 << PD3):\t");
+  printBin((uint8_t) ((1 << PD2) | (1 << PD3)));
+
+  Serial.print("~(1 << PD2) | (1 << PD3):\t");
+  printBin((uint8_t) (~((1 << PD2) | (1 << PD3))));
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+
+  Serial.print("DDRD: \t\t");
+  printBin((uint8_t) DDRD);
+
+  DDRD = DDRD & ~((1 << PD2) | (1 << PD3)); //<--- TODO
+
+  Serial.print("DDRD: \t\t");
+  printBin((uint8_t) DDRD);
+ 
 
   // put your setup code here, to run once:
 
