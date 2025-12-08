c72904c146cc4b60e841cda35a6bcd93032be51f
Bernd Donner <bernd.donner@sabel.com>
2025-12-08 11:03:29 +0100

    Unterricht vom 1.12

---
+++ 2526_E4A_Semester/Arbeitsblatt1/Arbeitsblatt1.ino
@@ -0,0 +1,30 @@
+void setup() {
+  char* b;
+  uint16_t a = 0xa35c;
+  Serial.begin(9600);
+  Serial.println("Aufgabe 2.1:");
+
+  Serial.print("Größe in Bytes von der Variable a: ");
+  Serial.println(sizeof(a));
+  Serial.print("Größe in Bytes von dem Zeiger b: ");
+  Serial.println(sizeof(b));
+
+  Serial.println("Aufgabe 2.2:");
+
+  Serial.print("Inhalt der Adresse, auf die sich b bezieht: ");
+  Serial.println(*b);
+
+  Serial.println("Aufgabe 2.3:");
+
+  Serial.println((uint16_t) &a, HEX);
+  Serial.println((uint16_t) &b, HEX);
+
+  Serial.println("Test:");
+
+  Serial.println((uint8_t) *((uint8_t*) &a), HEX);
+  Serial.println((uint8_t) *((uint8_t*) &a +1), HEX);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
