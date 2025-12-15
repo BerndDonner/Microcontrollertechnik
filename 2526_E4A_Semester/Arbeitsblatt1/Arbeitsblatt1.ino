4804ab352767f386815b43a15e1b0b22c66c041c
Bernd Donner <bernd.donner@sabel.com>
2025-12-15 14:55:40 +0100

    Unterricht vom 15.12

--- 2526_E4A_Semester/Arbeitsblatt1/Arbeitsblatt1.ino
+++ 2526_E4A_Semester/Arbeitsblatt1/Arbeitsblatt1.ino
@@ -1,13 +1,30 @@
+void mul_16(uint16_t* c) {
+  (*c) = (*c) * 16;
+}
+
+
 void setup() {
   char* b;
-  uint16_t a = 0xa35c;
+  uint16_t a = 0x235a;
+  char s[] = "E3A";
+
   Serial.begin(9600);
-  Serial.println("Aufgabe 2.1:");
+  Serial.println(F("Aufgabe 2.1:"));
 
   Serial.print("Größe in Bytes von der Variable a: ");
   Serial.println(sizeof(a));
   Serial.print("Größe in Bytes von dem Zeiger b: ");
   Serial.println(sizeof(b));
+  Serial.print("Größe in Bytes von dem String s: ");
+  Serial.println(sizeof(s));
+
+/*
+  Serial.println(s);
+  s[3] = '\0';
+  Serial.println(s);
+  s[3] = 'B';
+  Serial.println(s);
+*/
 
   Serial.println("Aufgabe 2.2:");
 
@@ -18,11 +35,22 @@ void setup() {
 
   Serial.println((uint16_t) &a, HEX);
   Serial.println((uint16_t) &b, HEX);
+  Serial.println((uint16_t) &s, HEX);
 
   Serial.println("Aufgabe 2.4:");
 
-  b = (char *) &a;  
+  b = (char *) &a;
+  Serial.println(*b);  
 
+  Serial.println("Aufgabe 2.5:");
+
+  Serial.println(*(b+1));  
+
+  Serial.println("Aufgabe 2.6:");
+
+  Serial.println(a);  
+  mul_16(&a);
+  Serial.println(a);  
 
 }
 
