06f0728a9cb908ec9a3c7de22cb8d40fa03a990d
Bernd Donner <bernd.donner@sabel.com>
2026-03-13 12:44:44 +0100

    Unterricht vom 12.03

--- 2526_E4A_Semester/5_minuten_ei/5_minuten_ei.ino
+++ 2526_E4A_Semester/5_minuten_ei/5_minuten_ei.ino
@@ -1,5 +1,13 @@
 #include "donner.h"
 
+/* 5.5min = 330s
+   330s : 16,384ms = 20141,60 = 20142
+*/
+
+constexpr uint16_t OVF_TARGET = 20142;
+
+volatile bool eier_fertig = false;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Jetzt geht's los!!!");
@@ -9,10 +17,19 @@ void setup() {
   //TCCR2A &= ~((1 << WGM21) | (1 << WGM20));
   TCCR2B |= (1 << CS20) | (1 << CS21) | (1 << CS22); //Prescaler 1024
   //TCCR2B &= ~(1 << WGM22);
-
   TCNT2 = 0;
+  TIMSK2 |= (1 << TOIE2); //Overflow Interrupt enable
 }
 
+
+ISR(TIMER2_OVF_vect) {
+  static uint16_t ovf = 0;
+  ovf++;
+  
+  if (ovf >= OVF_TARGET) {
+    eier_fertig = true;
+  }
+}
 
 
 // Trial Division (didaktisch simpel)
@@ -44,4 +61,12 @@ void workload_safe_prime() {
 void loop() {
   // Workload: so oft wie möglich rechnen, drucken nur bei Treffer
   workload_safe_prime();
+
+  if (eier_fertig) {
+    
+    TIMSK2 = 0; // disable all Timer2 interrupts
+    //TIMSK2 &= ~(1 << TOIE2); //Overflow Interrupt disable
+    
+    Serial.println("Schwing Deinen Arsch zum Herd!!!");
+  }
 }
