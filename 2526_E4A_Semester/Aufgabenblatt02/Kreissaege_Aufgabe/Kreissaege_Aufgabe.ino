e991a53f07b91ea90efae718db2153be9b72a21d
Bernd Donner <bernd.donner@sabel.com>
2026-05-07 08:00:49 +0200

    Übung für Kurzarbeit

---
+++ 2526_E4A_Semester/Aufgabenblatt02/Kreissaege_Aufgabe/Kreissaege_Aufgabe.ino
@@ -0,0 +1,35 @@
+const char* msg1 = "Ich säge Holz.";
+const char* msg2 = "Ich säge Metall.";
+const char* msg3 = "Ich säge Hand.";
+
+const char* material[] = {
+  msg1, msg1, msg2, msg1, msg2,
+  msg2, msg3, msg1, msg2, msg3
+};
+
+uint16_t dauer[] = {
+  1111, 500, 2700, 2222, 500,
+  1777, 2432, 678, 986, 1456
+};
+
+constexpr uint8_t ANZAHL = 10;
+constexpr uint8_t notaus_pin = 2;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(notaus_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(notaus_pin), stopp, FALLING);
+}
+
+void stopp() {
+  Serial.println("Säge wird sofort gestoppt");
+  Serial.flush();
+  abort();
+}
+
+void loop() {
+  for (uint8_t i = 0; i < ANZAHL; i++) {
+    Serial.println(material[i]);
+    delay(dauer[i]);
+  }
+}
